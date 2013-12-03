b9d0c4df0b9fb058f9661f1a3e88497023a091f6
andrej fritz <a.fritz@betzold.de>
2013-12-03 12:20:08 +0100

    chartJS for visual fancy

--- client/coffee/client.coffee
+++ client/coffee/client.coffee
@@ -5,5 +5,7 @@ Session.set('home.loading', true)
 
 Meteor.subscribe 'all_logs', ()->
   Session.set('home.loading', false)
+
 Meteor.subscribe 'all_settings'
+
 Session.setDefault 'editSet', ''
---
+++ startup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+read -p "Run in production mode?" answer
+
+while true
+do
+  case $answer in
+   [yY]* ) mrt --settings settings.json --production
+           break;;
+
+   [nN]* ) mrt --settings settings.json
+           exit;;
+
+   * )     echo "Dude, just enter Y or N, please."; break ;;
+  esac
+done
